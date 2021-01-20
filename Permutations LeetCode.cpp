0325a008c6b25acb8d925cbbbd3c19445d767e88
Swarnima Shishodia <swarnimashishodia11@gmail.com>
2021-01-20 18:10:52 +0530

    Create Permutations LeetCode.cpp

---
+++ Permutations LeetCode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void permuterecursive(vector<int>&nums,int begin,vector<vector<int>>&result)
+    {
+        if(begin==nums.size()-1)
+        {
+           result.push_back(nums); 
+            return;
+        }
+        for (int i = begin; i < nums.size(); i++) {
+		    swap(nums[begin], nums[i]);
+		    permuterecursive(nums, begin + 1, result);
+		    swap(nums[begin], nums[i]);
+		}
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>result;
+        permuterecursive(nums,0,result);
+        return result;
+        
+    }
+};
