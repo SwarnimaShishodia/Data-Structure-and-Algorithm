dd422a283de599ed191b252f2aa488ebd532f1d2
Swarnima Shishodia <swarnimashishodia11@gmail.com>
2021-07-12 23:11:58 +0530

    Update Bubble_Sort.cpp

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -71,3 +71,34 @@ swap(&arr[j], &arr[j - 1]);
 }
 } 
 }
+
+
+
+
+
+
+________________________________________________________________________________________________________________________________________
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int arr[5]={9,2,3,0,1};
+    
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            swap(arr[j],arr[j+1]);
+        }
+    }
+    
+    for(int i=0;i<n;i++)
+    cout<<arr[i];
+    
+    return 0;
+}
